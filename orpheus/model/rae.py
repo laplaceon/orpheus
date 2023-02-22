9d1bd6f58ab97ee4cb95befa01609ee755fabe42
Riyadh <8844262+laplaceon@users.noreply.github.com>
2023-02-22 15:46:58 -0500

    polyphase setting added

--- orpheus/model/rae.py
+++ orpheus/model/rae.py
@@ -15,11 +15,12 @@ class Orpheus(nn.Module):
         scales=(None, 4, 4, 4),
         blocks_per_stages=(1, 1, 1, 1),
         layers_per_blocks=(3, 3, 3, 3),
-        se_ratio=0.25
+        se_ratio=0.25,
+        fast_recompose=True
     ):
         super().__init__()
 
-        self.pqmf = PQMF(h_dims[0], 100)
+        self.pqmf = PQMF(h_dims[0], 100, fast_recompose)
 
         self.encoder = Encoder(h_dims, latent_dim, scales, blocks_per_stages, layers_per_blocks, se_ratio)
         self.decoder = Decoder(h_dims[::-1], latent_dim, scales[::-1], blocks_per_stages[::-1], layers_per_blocks[::-1], se_ratio)
